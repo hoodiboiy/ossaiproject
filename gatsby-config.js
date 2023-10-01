1f8982002122f0c943f4b830df5c9e1d0a99eae4
ossaichidozie <112661257+hoodiboiy@users.noreply.github.com>
2023-10-01 18:09:27 +0300

    ossaiproject

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    siteUrl: `http://hoodiboiy.github.io/ossaiproject`,
   },
   plugins: [
     {
